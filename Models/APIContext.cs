200f7529a47fc98a9d4c05307be0ef72ff388650
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-11-01 11:08:43 +0200

    add adults model

--- Models/APIContext.cs
+++ Models/APIContext.cs
@@ -7,7 +7,7 @@ namespace ChurchSystem.Models
     public class APIContext : DbContext
     {
         public APIContext(DbContextOptions<APIContext> options) : base(options) { }
-
+        public DbSet<Adults> Adults => Set<Adults>();
         public DbSet<Youths> Youths => Set<Youths>();
         public DbSet<Children> Children => Set<Children>();
         public DbSet<Tithe> Tithe => Set<Tithe>();
